2a30410ef93962630235200cd94be8a6a37ebe62
Miguel Celis <60907205+mcelis025@users.noreply.github.com>
2020-03-31 16:44:25 -0600

    added onclick gif url retreival

---
+++ assets/javascript/app.js
@@ -0,0 +1,34 @@
+var gifs = ["tiger king", "ronaldo", "hello", "bye"];
+
+$(document).on("click", ".gifImg", function () {
+
+    var gifImg = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=FUUB7nICE9mEG19RmZIIKCktFrZgWxyP&q=" + gifImg + "&limit=10&offset=0&lang=en";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+
+        var results = response.data;
+
+        for (var i = 0; i < results.length; i++) {
+
+            var gifDiv = $("<div>");
+            var p = $("<p>");
+            $(p).text("Rating: " + results[i].rating);
+            var gifImage = $("<img>");
+            $(gifImage).attr("src", results[i].images.fixed_height_still.url);
+
+            $(gifImage).attr('data-still', results[i].images.fixed_height_still.url);
+            $(gifImage).attr('data-animate', results[i].images.fixed_height.url);
+            $(gifImage).attr('data-state', "still");
+            $(gifImage).addClass("gif");
+
+
+            $(gifDiv).append(p, gifImage);
+            $("#gifView").prepend(gifDiv);
+        }
+    });
+});
